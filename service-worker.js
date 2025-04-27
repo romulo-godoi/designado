611a3f9135c1dd452220afe707757a7940eb5ec9
Rômulo <96986931+romulo-godoi@users.noreply.github.com>
2025-04-27 08:28:33 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Version 1.1 - Added calendar transition and language switcher functionality.
-const CACHE_NAME = 'jw-assignments-cache-v1.5'; // Incremented version
+const CACHE_NAME = 'jw-assignments-cache-v1.6'; // Incremented version
 
 const urlsToCache = [
   './', // Cache the root path
